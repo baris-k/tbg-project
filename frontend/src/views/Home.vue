32cb0ca3524bef7a67be2ba3b7af6b8811943757
baris-k <bariskoran@icloud.com>
2022-02-14 14:21:53 +0100

    add frontend with VUE

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App") 
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users')
+
+    this.users = usersRequest.data
+  },
+}
+</script>
